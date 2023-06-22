715165df866a3b5e965d2574b10fd70b49957c0f
sandu cristian <sandu_cristian2007@yahoo.com>
2023-06-22 21:33:12 +0300

    Add testing reporting

--- src/test/java/org/skillbrain/DemoQaTutorial.java
+++ src/test/java/org/skillbrain/DemoQaTutorial.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageObjects.DemoQaPage;
 
@@ -62,54 +63,54 @@ public class DemoQaTutorial {
         System.out.println("Test finished without error");
     }
 
-    @Test(dependsOnMethods = "demoQaLoginTest", testName = "Complete practice form with valid data")
-    public void demoQaFormTest() throws InterruptedException {
-        try {
-            // tag name -> div, header, strong, a
-            WebElement header = driver.findElement(By.tagName("header"));
-//        header.click();
-            // css -> class -> .className  (class property)
-            // xpath -> //tagName[@property='value'] ->
-            WebElement formsCard = driver.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[2]"));
-            formsCard.click();
-            WebElement practiceForm = driver.findElement(By.xpath("(//li[@id='item-0'])[2]"));
-            practiceForm.click();
-            currentUrl = driver.getCurrentUrl();
-            title = driver.getTitle();
-            Assert.assertEquals(currentUrl, "https://demoqa.com/automation-practice-form");
-            driver.findElement(By.id("firstName")).sendKeys("Cristian");
-            driver.findElement(By.id("lastName")).sendKeys("Sandu");
-            driver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
-            driver.findElement(By.xpath("//label[@for='gender-radio-1']")).click();
-            driver.findElement(By.id("userNumber")).sendKeys("0123456789");
-            WebElement calendar = driver.findElement(By.id("dateOfBirthInput"));
-            calendar.click();
-            WebElement selectMonth = driver.findElement(By.cssSelector(".react-datepicker__month-select"));
-            Select selectBirthMonth = new Select(selectMonth);
-            selectBirthMonth.selectByIndex(4);
-            WebElement selectYear = driver.findElement(By.cssSelector(".react-datepicker__year-select"));
-            Select selectBirthYear = new Select(selectYear);
-            selectBirthYear.selectByValue("1993");
-            driver.findElement(By.cssSelector(".react-datepicker__day.react-datepicker__day--015")).click();
-            WebElement subjects = driver.findElement(By.id("subjectsInput"));
-            subjects.sendKeys("Math");
-            subjects.sendKeys(Keys.RETURN);
-            subjects.sendKeys("Acc");
-            subjects.sendKeys(Keys.RETURN);
-            driver.findElement(By.xpath("//label[@for='hobbies-checkbox-1']")).click();
-            driver.findElement(By.id("currentAddress")).sendKeys("Home address");
-            WebElement state = driver.findElement(By.id("react-select-3-input"));
-            state.sendKeys("Uttar");
-            state.sendKeys(Keys.RETURN);
-            WebElement city = driver.findElement(By.id("react-select-4-input"));
-            city.sendKeys("Agra");
-            city.sendKeys(Keys.RETURN);
-        } catch (NoSuchElementException | ElementNotInteractableException |
-                 StaleElementReferenceException e) {
-            String errorMessage = e.getMessage();
-            System.out.println(errorMessage);
-        }
-    }
+//    @Test(dependsOnMethods = "demoQaLoginTest", testName = "Complete practice form with valid data")
+//    public void demoQaFormTest() throws InterruptedException {
+//        try {
+//            // tag name -> div, header, strong, a
+//            WebElement header = driver.findElement(By.tagName("header"));
+////        header.click();
+//            // css -> class -> .className  (class property)
+//            // xpath -> //tagName[@property='value'] ->
+//            WebElement formsCard = driver.findElement(By.xpath("(//div[@class='card mt-4 top-card'])[2]"));
+//            formsCard.click();
+//            WebElement practiceForm = driver.findElement(By.xpath("(//li[@id='item-0'])[2]"));
+//            practiceForm.click();
+//            currentUrl = driver.getCurrentUrl();
+//            title = driver.getTitle();
+//            Assert.assertEquals(currentUrl, "https://demoqa.com/automation-practice-form");
+//            driver.findElement(By.id("firstName")).sendKeys("Cristian");
+//            driver.findElement(By.id("lastName")).sendKeys("Sandu");
+//            driver.findElement(By.id("userEmail")).sendKeys("test@gmail.com");
+//            driver.findElement(By.xpath("//label[@for='gender-radio-1']")).click();
+//            driver.findElement(By.id("userNumber")).sendKeys("0123456789");
+//            WebElement calendar = driver.findElement(By.id("dateOfBirthInput"));
+//            calendar.click();
+//            WebElement selectMonth = driver.findElement(By.cssSelector(".react-datepicker__month-select"));
+//            Select selectBirthMonth = new Select(selectMonth);
+//            selectBirthMonth.selectByIndex(4);
+//            WebElement selectYear = driver.findElement(By.cssSelector(".react-datepicker__year-select"));
+//            Select selectBirthYear = new Select(selectYear);
+//            selectBirthYear.selectByValue("1993");
+//            driver.findElement(By.cssSelector(".react-datepicker__day.react-datepicker__day--015")).click();
+//            WebElement subjects = driver.findElement(By.id("subjectsInput"));
+//            subjects.sendKeys("Math");
+//            subjects.sendKeys(Keys.RETURN);
+//            subjects.sendKeys("Acc");
+//            subjects.sendKeys(Keys.RETURN);
+//            driver.findElement(By.xpath("//label[@for='hobbies-checkbox-1']")).click();
+//            driver.findElement(By.id("currentAddress")).sendKeys("Home address");
+//            WebElement state = driver.findElement(By.id("react-select-3-input"));
+//            state.sendKeys("Uttar");
+//            state.sendKeys(Keys.RETURN);
+//            WebElement city = driver.findElement(By.id("react-select-4-input"));
+//            city.sendKeys("Agra");
+//            city.sendKeys(Keys.RETURN);
+//        } catch (NoSuchElementException | ElementNotInteractableException |
+//                 StaleElementReferenceException e) {
+//            String errorMessage = e.getMessage();
+//            System.out.println(errorMessage);
+//        }
+//    }
 
     @Test(testName = "Login to demoqa page with valid user")
     public void demoQaLoginTest() throws InterruptedException {
@@ -118,6 +119,23 @@ public class DemoQaTutorial {
         currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(title, "DEMOQA");
         Assert.assertEquals(currentUrl,"https://demoqa.com/");
+    }
+
+    @DataProvider(name = "myDataProvider")
+    public Object[][] myDataProvider() {
+        return new Object[][] {
+                {"elements"},
+                {"forms"},
+                {"alertsWindows"},
+                {"books"},
+                {"widgets"}
+        };
+    }
+
+    @Test(dataProvider = "myDataProvider",testName = "Testing different URLs for demoqa page")
+    public void demoQaTestUrls(String page) {
+        driver.get("https://demoqa.com/" + page);
+        System.out.println(page);
     }
 
 
