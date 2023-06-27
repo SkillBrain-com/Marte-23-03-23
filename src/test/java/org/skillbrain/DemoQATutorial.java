2adf9142e819cae8a88bbabe71fc5ea15a0b3ac2
MarcoPolo236 <mark_hero13@yahoo.com>
2023-06-27 21:45:10 +0300

    demoqa

--- src/test/java/org/skillbrain/DemoQATutorial.java
+++ src/test/java/org/skillbrain/DemoQATutorial.java
@@ -3,6 +3,7 @@ package org.skillbrain;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class DemoQATutorial {
     public static void main(String[] args) throws InterruptedException {
@@ -16,7 +17,46 @@ public class DemoQATutorial {
         WebElement formsCard = driver.findElement(By.xpath("//div[@class='card mt-4 top-card'] [2]"));
         formsCard.click();
 
-        driver.quit();
+        WebElement practiceForms = driver.findElement(By.xpath("(//li[@id='item-0'])[2]"));
+        practiceForms.click();
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.sendKeys("Alecu");
+
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.sendKeys("Marco");
+
+        WebElement userEmail = driver.findElement(By.id("userEmail"));
+        userEmail.sendKeys("mark_hero13@yahoo.com");
+
+        WebElement radioButton = driver.findElement(By.xpath("//label[@for='gender-radio-1']"));
+        radioButton.click();
+
+        WebElement userNumber = driver.findElement(By.id("userNumber"));
+        userNumber.sendKeys("0729663849");
+
+        WebElement selectBirthYear = driver.findElement(By.id("dateOfBirthInput"));
+        selectBirthYear.click();
+        WebElement select = driver.findElement(By.cssSelector(".react-datepicker__month-select"));
+        org.openqa.selenium.support.ui.Select selectMonth = new org.openqa.selenium.support.ui.Select(select);
+        selectMonth.selectByIndex(8);
+
+        WebElement selectYear = driver.findElement(By.cssSelector(".react-datepicker__year-select"));
+        Select selectYear2 = new Select(selectYear);
+        selectYear2.selectByValue("2000");
+
+
+        
+
+
+
+
+
+
+
+
+
+
 
 
         // xpath -->   //TagName[@property='value']  -->
@@ -47,6 +87,6 @@ public class DemoQATutorial {
 
 
 
-        driver.quit();
+
     }
 }
